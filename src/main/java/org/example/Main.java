65b398ff12214716e122e87afd63dbc6941f30b6
rishavemail786 <rishavemail786@gmail.com>
2022-09-09 12:40:34 +0530

    JDBC DEMO

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,37 @@
-package org.example;public class Main {
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url="jdbc:mysql://localhost:3306/CarSales";
+            String user= "root";
+            String pass= "root@123";
+            Connection connection = DriverManager.getConnection(url,user,pass);
+            if (connection != null){
+                System.out.println("connection success");
+                //statement
+                Statement st = connection.createStatement();
+                String sql="Insert into SalesPerson values(1235,'rishav','London',15)";
+                int row=st.executeUpdate(sql);
+                if (row==1){
+                    System.out.println("data inserted");
+                }else {
+                    System.out.println("data not inserted");
+                }
+            }else {
+                System.out.println("not connected");
+            }
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
